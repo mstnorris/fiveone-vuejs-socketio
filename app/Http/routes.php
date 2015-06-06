633cc6b19a71dfdcf47b549fa63baaca10943665
Michael Norris <mike@michaelnorris.co.uk>
2015-06-06 14:30:46 +0100

    added guestbook and other routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,28 +1,39 @@
 <?php
 
+// auth()->loginUsingId(1);
+
 get('/', function() {
     return response()->redirectTo('guestbook');
 });
 
-get('guestbook', function() {
-    return view('guestbook');
-});
+Route::group(['middleware' => 'auth'], function () {
 
-get('api/messages', function() {
-    return App\Message::all();
-});
+    get('guestbook', function() {
+        return view('guestbook');
+    });
 
-post('api/messages', function() {
-    return App\Message::create(Request::all());
-});
+    get('messages', 'MessagesController@index');
 
-get('fire', function () {
-    event(new App\Events\MyEventNameHere());
-    return "event fired";
-});
+    get('users', 'UsersController@index');
 
-get('test', function () {
-    return view('test');
+    get('api/messages', function() {
+        return SomethingGoodHappenedToday\Message::all();
+    });
+
+    post('api/messages', function() {
+        return SomethingGoodHappenedToday\Message::create(Request::all());
+    });
+
+
+    get('fire', function () {
+        event(new SomethingGoodHappenedToday\Events\MyEventNameHere());
+        return "event fired";
+    });
+
+    get('test', function () {
+        return view('test');
+    });
+
 });
 
 // Authentication routes...
